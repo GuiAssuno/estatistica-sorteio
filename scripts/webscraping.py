75a58934d500f88fafcdc01f0596e56f4abf834c
Guilherme Assunção <Gui_ab_@hotmail.com>
2026-03-18 11:45:12 -0300

    Improve web scraping function documentation and structure
    Refactor web scraping functions for clarity and consistency.

--- scripts/webscraping.py
+++ scripts/webscraping.py
@@ -7,20 +7,16 @@ import pathlib
 salvar = gn.gerador_de_dezenas()
 
 
-# Solução simples e direta
 def separar_registros(dados, format):
     '''função para filtrar formatação especifica do codigo'''
-    # filtra apenas o conteudo chave     
     partes = re.findall(format, dados)
     
     return partes
 
 
-
 def coletar_dados_site(url, tag_alvo, classe_alvo):
     """
-    Função genérica para fazer web scraping em um site.
-    
+    Função para fazer web scraping em um site.
     :url: A URL completa do site que você quer analisar.
     :tag_alvo: A tag HTML onde o dado está (ex: 'h2', 'a', 'div').
     :classe_alvo: A classe CSS do elemento para filtrar (ex: 'post-title').
@@ -28,8 +24,6 @@ def coletar_dados_site(url, tag_alvo, classe_alvo):
     """
     
     print(f"Iniciando a coleta de dados da URL: {url}")
-    
-    # Lista para armazenar os dados coletados
     dados_coletados = []
 
     try:
@@ -37,23 +31,20 @@ def coletar_dados_site(url, tag_alvo, classe_alvo):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
         response = requests.get(url, headers=headers, timeout=10)
         
-        # Verifica se a requisição foi bem-sucedida (código 200)
+        # Verifica se a requisição
         response.raise_for_status() 
         print("Página baixada com sucesso!")
 
-        # 2. ANALISAR O CONTEÚDO HTML
+
         site = BeautifulSoup(response.text, 'lxml')
         print("HTML analisado. Procurando pelos dados...")
 
-        # 3. LOCALIZAR OS DADOS
-        # Encontra TODOS os elementos que correspondem à tag e classe fornecidas.
         elementos_encontrados = site.find_all(tag_alvo, class_=classe_alvo)
         
         if not elementos_encontrados:
             print(f"Atenção: Nenhum elemento encontrado com a tag '{tag_alvo}' e classe '{classe_alvo}'. Verifique o site.")
             return []
-        
-        # 4. EXTRAIR A INFORMAÇÃO
+
         padrao = r"\d{2}(?: \d{2}){5}"
         
         for elemento in elementos_encontrados:
